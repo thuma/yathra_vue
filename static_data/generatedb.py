1b3199664e9b818ccd380c723e1efb47c7a638af
Martin Thuresson <martin.thure@gmail.com>
2019-06-11 06:58:12 +0200

    Added gtfs tables

--- static_data/generatedb.py
+++ static_data/generatedb.py
@@ -6,6 +6,9 @@ stops.execute("CREATE TABLE IF NOT EXISTS stops(stop_id INT, stop_name TEXT ,sto
 stops.execute("CREATE UNIQUE INDEX IF NOT EXISTS id ON stops(stop_id);")
 stops.execute("CREATE TABLE IF NOT EXISTS astops(agency_id INT,stop_id INT, agency_stop_id INT);")
 stops.execute("CREATE INDEX IF NOT EXISTS aid ON astops(agency_id, stop_id);")
+stops.execute("CREATE TABLE IF NOT EXISTS stop_times(trip_id INT,arrival_time TEXT,departure_time TEXT,stop_id INT,stop_sequence INT,pickup_type INT,drop_off_type INT);")
+stops.execute("CREATE TABLE IF NOT EXISTS trips(route_id INT,service_id INT,trip_id INT,trip_headsign TEXT,trip_short_name TEXT);")
+stops.execute("CREATE TABLE IF NOT EXISTS routes(route_id INT,agency_id INT,route_short_name TEXT,route_long_name TEXT,route_type INT,route_url TEXT);")
 stops.execute("DELETE FROM stops;")
 stops.execute("DELETE FROM astops;")
 stops.execute("VACUUM")
@@ -13,3 +16,6 @@ print "sqlite3 stops"
 print '.separator ","'
 print '.import exstops.txt stops'
 print '.import agency_stops.txt astops'
+print '.import stop_times.txt stop_times'
+print '.import trips.txt trips'
+print '.import routes.txt routes'
